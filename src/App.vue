bb66a344659dc4de4cab657928b209b3a8843561
syougo <supernaturalsyougo@gmail.com>
2023-02-16 00:23:42 +0900

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CommonHeader header-title="a"></CommonHeader>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CommonHeader from '@/components/CommonHeader.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CommonHeader,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin: 0;
+  }
 </style>
