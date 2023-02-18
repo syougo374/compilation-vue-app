96ca229b06052849bd65a4d9132c86dbdd9e4a8e
syougo <supernaturalsyougo@gmail.com>
2023-02-18 23:51:21 +0900

    create footer complited

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <CommonHeader header-title="a"></CommonHeader>
-
+  <CommonFooter></CommonFooter>
 </template>
 
 <script>
 import CommonHeader from '@/components/molecules/CommonHeader.vue';
+import CommonFooter from '@/components/molecules/CommonFooter'
 export default {
   name: 'App',
   components: {
     CommonHeader,
+    CommonFooter,
   }
 }
 </script>
